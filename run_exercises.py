dad06de2ac7f7a02a0d1541c757e88f6c54dab44
Jeff Pape Careers <jeff.pape.careers@gmail.com>
2026-04-17 15:58:10 +0000

    added imports

--- run_exercises.py
+++ run_exercises.py
@@ -1,2 +1,6 @@
+import portfolio.data
+from portfolio.data import create_portfolio
+from portfolio.report import print_report
+
 my_portfolio = portfolio.data.create_portfolio("Retirement")
 portfolio.report.print_report(my_portfolio)
