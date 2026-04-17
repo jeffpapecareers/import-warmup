89ba70a56145aac5399a4b73c946a556b6da082c
Jeff Pape Careers <jeff.pape.careers@gmail.com>
2026-04-17 16:36:56 +0000

    add __init__.py

---
+++ portfolio/__init__.py
@@ -0,0 +1,2 @@
+from portfolio.data import create_portfolio
+from portfolio.report import print_report
--- run_exercises.py
+++ run_exercises.py
@@ -1,6 +1,5 @@
 import portfolio.data
-from portfolio.data import create_portfolio
-from portfolio.report import print_report
 
-my_portfolio = portfolio.data.create_portfolio("Retirement")
-portfolio.report.print_report(my_portfolio)
+if __name__ == "__main__":
+    my_portfolio = portfolio.data.create_portfolio("Retirement")
+    portfolio.report.print_report(my_portfolio)
